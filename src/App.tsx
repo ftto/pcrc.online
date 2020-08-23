8cf17c3db7ec530caf7609f2239e73d39dc2cc1e
Hoony Chang <the6thm0nth@Hoonys-MacBook-Pro.local>
2020-08-23 21:22:08 +0900

    update main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [joinUrl, setJoinUrl] = useState("");
+  const [status, setStatus] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.pcrc.online/status")
+      .then((res) => res.json())
+      .then((res) => {
+        setJoinUrl(res.joinUrl);
+        setStatus(res.status);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      status: {status}
+      <br />
+      {status === "started" && <a href={joinUrl}>참여하기</a>}
     </div>
   );
 }
