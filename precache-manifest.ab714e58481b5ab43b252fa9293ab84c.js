458fa9116f03895930d96da95e96d8cb107ac9f5
Hoony Chang <the6thm0nth@Hoonys-MacBook-Pro.local>
2020-08-23 21:25:16 +0900

    Updates

---
+++ precache-manifest.ab714e58481b5ab43b252fa9293ab84c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa2d31ed9a54fdadea9a2ab94e560e80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ea4bf24b5823be297b8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "79b391c0e557e9721486",
+    "url": "/static/js/2.6905652c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6905652c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ea4bf24b5823be297b8",
+    "url": "/static/js/main.b9c07d91.chunk.js"
+  },
+  {
+    "revision": "be686786facc18b6d6d0",
+    "url": "/static/js/runtime-main.fb4781c7.js"
+  }
+]);
